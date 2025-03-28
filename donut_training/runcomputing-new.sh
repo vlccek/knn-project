50498716bdc48e4256106268e6c334b8ffa54a96
jvlk <kuba24862vlk@gmail.com>
2025-03-28 17:43:37 +0100

    add new version of training scripts

--- donut_training/runcomputing-new.sh
+++ donut_training/runcomputing-new.sh
@@ -1,61 +1,77 @@
 #!/bin/bash
 #PBS -N KNN-DONUT-TRAINING
-#PBS -l walltime=00:07:0
+#PBS -l walltime=02:10:00
 #PBS -q gpu@pbs-m1.metacentrum.cz
-#PBS -l select=1:ncpus=4:ngpus=1:mem=128gb:gpu_mem=16gb:scratch_ssd=40000mb
+#PBS -l select=1:ncpus=9:ngpus=1:mem=128gb:gpu_mem=43gb:scratch_ssd=400000mb
 
-
-# Vytvoření unikátního adresáře na scratch
+# Creating a unique directory on scratch
 SCRATCH_DIR=/scratch.ssd/xvlkja07/job_${PBS_JOBID}
 echo "SCRATCH_DIR: ${SCRATCH_DIR}"
 # mkdir -p ${SCRATCH_DIR}
 
-# Zkopírujeme pouze složky donut a dataset_creating_json pomocí rsync
+# Copy only the 'donut' folder and the dataset file from 'dataset_creating_json' using rsync
 rsync -avP /storage/brno2/home/xvlkja07/KNN/donut ${SCRATCH_DIR}/
 rsync -avzP /storage/brno2/home/xvlkja07/KNN/dataset_creating_json/dataset.tar.gz ${SCRATCH_DIR}/donut
 
 cd ${SCRATCH_DIR}/donut/
 
-echo "Extrahuji dataset.tar.gz"
+echo "Extracting dataset.tar.gz"
 pigz -dc dataset.tar.gz | tar xf -
 
-#list folder content
+# Listing folder contents
 ls -laR ${SCRATCH_DIR}
 
-# Inicializace prostředí
+# Initializing environment
 # source /storage/brno2/home/xvlkja07/.bashrc
 module add mambaforge
 
-# Vytvoření nového conda prostředí z YAML souboru do složky na scratch
+# Creating a new conda environment from the YAML file in the scratch folder
 # mamba env create -f /storage/brno2/home/xvlkja07/KNN/donut_training/conda-knn-donut.yml --prefix ${SCRATCH_DIR}/knn-donut
 
-# Aktivace nově vytvořeného prostředí
+# Activating the newly created environment
 mamba activate /storage/brno2/home/xvlkja07/KNN/donut_training/knn-donut
 
-# Nastavení pracovního adresáře – budeme pracovat ze složky donut
+# Setting the working directory – we will operate from the 'donut' folder
 cd ${SCRATCH_DIR}/donut
 
+# Define source and destination for results copying
+RESULT_SOURCE=${SCRATCH_DIR}/donut/result/
+RESULT_DEST=/storage/brno2/home/xvlkja07/KNN/donut_training/result/
 
-# Zápis informace o spuštění do logu
+# Logging job start information
 echo "$PBS_JOBID is running on node $(hostname -f)" >> /storage/brno2/home/xvlkja07/KNN/donut_training/jobs_info.txt
 
-echo "Model bude uložen do $(pwd)"
-echo "Spouštím Trénování z prostředí na scratch"
+echo "Model will be saved to $(pwd)"
+echo "Starting training from the scratch environment"
 
-echo "Spouštím train.py --config 'config/train_cord.yaml' \
-                --pretrained_model_name_or_path naver-clova-ix/donut-base' \
-                --dataset_name_or_paths ../dataset/ \
-                --exp_version test_experiment "
-# Spuštění tréninku
+# Start a background process that copies the results every 10 minutes
+(
+  while true; do
+    echo "Periodically copying results from ${RESULT_SOURCE} to ${RESULT_DEST}..."
+    mkdir -p $(dirname ${RESULT_DEST})
+    rsync -av ${RESULT_SOURCE} ${RESULT_DEST}
+    sleep 600  # Wait for 10 minutes
+  done
+) &
+COPY_PID=$!
+
+echo "Starting train.py --config 'config/train_cord.yaml' \
+                --pretrained_model_name_or_path naver-clova-ix/donut-base \
+                --dataset_name_or_paths ['./dataset/'] \
+                --exp_version test_experiment"
+# Running training
 python train.py --config "config/train_cord.yaml" \
                 --pretrained_model_name_or_path "naver-clova-ix/donut-base" \
                 --dataset_name_or_paths "['./dataset/']" \
                 --exp_version "test_experiment"
 
-# Po skončení tréninku zkopírujeme složku s výsledky zpět do úložiště pomocí rsync
-RESULT_SOURCE=${SCRATCH_DIR}/donut/knn-donut/result
-RESULT_DEST=/storage/brno2/home/xvlkja07/KNN/donut_training/knn-donut/result
 
-echo "Kopíruji výsledky z ${RESULT_SOURCE} do ${RESULT_DEST}"
+
+# After training, kill the periodic copying process
+echo "Training finished. Stopping periodic results copying process (PID: ${COPY_PID})."
+kill $COPY_PID
+
+# Optionally, do one final copy of the results
+echo "Final copy of results from ${RESULT_SOURCE} to ${RESULT_DEST}"
 mkdir -p $(dirname ${RESULT_DEST})
 rsync -av ${RESULT_SOURCE} ${RESULT_DEST}
--- donut_training/runcomputing.sh
+++ donut_training/runcomputing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N KNN-DONUT-TRAINING
-#PBS -l walltime=4:0:0
+#PBS -l walltime=8:0:0
 #PBS -q gpu@pbs-m1.metacentrum.cz
 #PBS -l select=1:ncpus=4:ngpus=1:mem=128gb:gpu_mem=16gb:scratch_local=400mb
 
