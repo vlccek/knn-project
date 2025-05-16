9b57478aa2aadf843b5ea51f53b9c17db48ee715
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2025-05-16 22:09:58 +0200

    Update README, add Layout training script

--- pero_layout/datasets/create_dataset.py
+++ pero_layout/datasets/create_dataset.py
@@ -13,15 +13,11 @@ label_mapping = {
 }
 
 def load_files_to_skip(filename='files_to_skip.txt'):
-    """Načte seznam souborů, které se mají přeskočit, ze souboru."""
     with open(filename, 'r', encoding='utf-8') as f:
         files_to_skip = [line.strip() for line in f.readlines()]
     return files_to_skip
 
 def convert_labelstudio_bbox_to_pixels(bbox, original_width, original_height):
-    """
-    Convert Label Studio bounding box (normalized coordinates) to pixel coordinates.
-    """
     x1 = bbox['x']/100 * original_width
     y1 = bbox['y']/100 * original_height
     x2 = x1 + bbox['width']/100 * original_width
@@ -40,9 +36,6 @@ def calculate_overlap(bbox1, bbox2):
     y1_bbox = bbox2['y1']
     y2_bbox = bbox2['y2']
 
-
-    
-    # Výpočet překrytí dvou boxů
     x_overlap = max(0, min(x2, x2_bbox) - max(x1, x1_bbox))
     y_overlap = max(0, min(y2, y2_bbox) - max(y1, y1_bbox))
     
@@ -50,7 +43,6 @@ def calculate_overlap(bbox1, bbox2):
     area1 = (x2 - x1) * (y2 - y1)
     area2 = (x2_bbox - x1_bbox) * (y2_bbox - y1_bbox)
     
-    # Vratit poměr překrytí k celkové ploše
     return overlap_area / min(area1, area2)
 
 def get_best_matching_annotation(token_bbox, annotations):
@@ -68,7 +60,6 @@ def get_best_matching_annotation(token_bbox, annotations):
 
 
 def parse_ocr_xml(ocr_file):
-    """Parse the OCR XML file and extract tokens and bounding boxes."""
     tree = ET.parse(ocr_file)
     root = tree.getroot()
     
@@ -93,7 +84,6 @@ def parse_ocr_xml(ocr_file):
     return tokens, bboxes, maxheight, maxwidth
 
 def parse_labelstudio_json(labelstudio_file):
-    """Parse the Label Studio JSON file and extract the annotations."""
     with open(labelstudio_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
     
@@ -121,28 +111,20 @@ def parse_labelstudio_json(labelstudio_file):
     return annotations
 
 def normalize_bboxes(bboxes, original_width, original_height):
-    """
-    Normalizuje seznam bounding boxů podle původní šířky a výšky obrázku.
-    Pokud jsou x1 a x2 obě záporné, nastaví je na [0, 1] (nulová plocha).
-    Pokud je nějaká hodnota menší než 0, nastaví ji na 0.
-    """
     normalized_bboxes = []
     for bbox in bboxes:
         x1, y1, x2, y2 = bbox
         
-        # Normalizace
         normalized_x1 = int(1000 * x1 / original_width)
         normalized_y1 = int(1000 * y1 / original_height)
         normalized_x2 = int(1000 * x2 / original_width)
         normalized_y2 = int(1000 * y2 / original_height)
         
-        # Pokud jsou obě souřadnice x1 a x2 záporné, nastaví je na [0, 1]
         if normalized_x1 < 0 and normalized_x2 < 0:
             normalized_x1, normalized_x2 = 0, 1
         if normalized_y1 < 0 and normalized_y2 < 0:
             normalized_y1, normalized_y2 = 0, 1
         
-        # Pokud je hodnota menší než 0, nastaví ji na 0
         normalized_bbox = [
             max(0, normalized_x1),
             max(0, normalized_y1),
@@ -160,19 +142,15 @@ def generate_dataset(labelstudio_json, ocr_folder, files_to_skip):
     dataset = []
     i = 0
     for image_name, ann_data in annotations.items():
-        # Pokud je soubor na seznamu k přeskočení, přeskočíme ho
         if image_name not in files_to_skip or len(ann_data) == 0:
-            if(len(ann_data) == 0):
-                print("Skipping empty annotations.")
-            print(f"Soubor {image_name} se přeskočí. 🚫")
+            print(f"Skipping file {image_name}.")
             continue
 
         ocr_file = os.path.join(ocr_folder, image_name.replace('.jpg', '.xml'))
         
-        # Zkontrolujeme, zda XML soubor existuje
         if not os.path.exists(ocr_file):
             print(f"Soubor {ocr_file} neexistuje. 🚫")
-            continue  # Pokračujeme na další obrázek, pokud soubor neexistuje
+            continue
         else:
             print(f"Soubor {ocr_file} existuje. ✅")
         
@@ -181,7 +159,6 @@ def generate_dataset(labelstudio_json, ocr_folder, files_to_skip):
             print("Skipping due to not matching max height or width")
             break
         
-        # Předpokládejme, že ann_data obsahuje anotace pro všechny bounding boxy na daném obrázku
         all_ann_bboxes = []
         
         for ann in ann_data:
@@ -189,9 +166,7 @@ def generate_dataset(labelstudio_json, ocr_folder, files_to_skip):
             original_width = ann['original_width']
             original_height = ann['original_height']
             
-            # Přepočet na souřadnice v pixelech
             ann_bbox = convert_labelstudio_bbox_to_pixels(ann_value, original_width, original_height)
-            # Přidáme do seznamu všech bounding boxů pro daný obrázek
             all_ann_bboxes.append(ann_bbox)
         
         ner_tags = []
@@ -202,7 +177,6 @@ def generate_dataset(labelstudio_json, ocr_folder, files_to_skip):
             else:
                 ner_tags.append(0)
         
-        # Vytvoření záznamu v datasetu pro tento obrázek
         dataset.append({
             "tokens": tokens,
             "bboxes": normalize_bboxes(bboxes, original_width, original_height),
@@ -213,29 +187,14 @@ def generate_dataset(labelstudio_json, ocr_folder, files_to_skip):
     return dataset
 
 def split_dataset(dataset, train_ratio=0.80, test_ratio=0.1, validation_ratio=0.1):
-    """Randomly splits the dataset into train, test, and validation sets, using only a small fraction (20%) of the dataset."""
+    random.shuffle(dataset)
     
-    # Zamíchání datasetu pro náhodné rozdělení
-    #random.shuffle(dataset)
-    
-    # Celkový počet záznamů v datasetu
     total_size = len(dataset)
     
-    # Určení velikosti každé sady podle zadaného poměru
-    fraction_size = int(1 * total_size)  # Pouze 20% celkového datasetu
+    fraction_size = int(1 * total_size)
     train_size = int(train_ratio * fraction_size)
     test_size = int(test_ratio * fraction_size)
     validation_size = fraction_size - train_size - test_size
-
-    
-    # train_data = dataset[0:100]  # Prvních 5 položek
-    # test_data = dataset[185:186]  # 6. až 10. položka (indexy 5 až 9)
-    # validation_data = dataset[201:300] 
-    # test_data = [record for record in test_data if len(record.get('tokens', [])) <= 512]
-    print(len(dataset))
-    # train_data = dataset[0:750]  # Prvních 5 položek
-    # test_data = dataset[751:830]  # 6. až 10. položka (indexy 5 až 9)
-    # validation_data = dataset[830:300] 
 
     train_data = dataset[0:train_size]
     test_data = dataset[train_size:train_size + test_size]
@@ -255,7 +214,6 @@ ocr_folder = './output/alto'
 dataset = generate_dataset(labelstudio_json, ocr_folder, files_to_skip)
 train_data, test_data, validation_data = split_dataset(dataset)
 
-# Uložení každého datasetu do samostatného souboru
 with open('dataset_train.json', 'w', encoding='utf-8') as f:
     json.dump(train_data, f, ensure_ascii=False, indent=4)
 
--- readme.md
+++ readme.md
@@ -14,3 +14,30 @@ This repository provides a comprehensive pipeline for creating datasets, trainin
 *   **`donut/`:** This Directory contain core model of donut which is a Document Understanding Transformer models and our extension of it. 
 
 *   **`donut_training/`:** The folder will contain training scripts for training on metacentrum and env. 
+
+## **`pero_layout`** 
+
+Folder containing all data and source codes for our PERO + LayoutLMv3 solution.
+*  `datasets` - contains datasets that were used for training (without images, since they are too big) along with the process for their creation.
+
+    * `convert_images.py` - converts all images to RGB format
+    * `create_dataset.py` - creates 3 dataset files, one for each split (80:10:10 ratio)
+    * `annotations.json` - image annotations downloaded from LabelStudio
+    * `created_datasets` folder containing datasets for train, test and validation splits.
+
+*  `training` - folder containing training script used for LayoutLMv3 finetuning on Google Colab.
+
+*  `pipeline` - folder containing whole final processing pipeline.
+    * `images` - images to be processed
+    * `processing` - folder containing `PeroProcessor.py` for processing PERO OCR outputs, `LayoutProcessor.py` for Layout inference and `ocr-to-json.py` controling the whole process and converting the outputs of LayoutLMv3 to JSON.
+    *  `requirements.txt`
+    *  `run.sh` - script for launching the processing of images in `images` folder
+
+
+## How to run PERO + LAYOUTLMv3 inference
+
+1. Download configured Layout and PERO models from [this url](https://drive.google.com/drive/folders/1TLxB4ENP5-d_-lFLbSLXGB39YVsoovVX?usp=sharing) (`models.tar.gz`) and place the TAR in the `pipeline` folder.
+
+2. `tar -xzvf models.tar.gz` 
+3. Make sure you have some images in `images` folder to be processed
+4. `./run.sh`
