4928f5b5089561b79f97a8169e436417c1a99637
Jakub Vlk <43584052+vlccek@users.noreply.github.com>
2025-05-17 02:06:00 +0200

    rm uncs. params

--- pero_layout/pipeline/run.sh
+++ pero_layout/pipeline/run.sh
@@ -1,12 +1,9 @@
 python3 ./pero-ocr/user_scripts/parse_folder.py  \
-         -i ./images \
-         --output-render-path ./output/renders/ \
-         --output-logit-path ./output/logit/ \
+         -i ../../img/validation \
          --output-alto-path ./output/alto \
-         --output-xml-path ./output/page \
          -c ./pero-ocr/engine/config.ini \
          --device cpu
 
-python3 ./processing/ocr-to-json.py ./output/alto/ ./images
+python3 ./processing/ocr-to-json.py ./output/alto/ ../../img/validation
 
-rm -rf ./output
+# rm -rf ./output
