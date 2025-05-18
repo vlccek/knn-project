0efff72075a061b8d8554a34304223605107a46b
jvlk <kuba24862vlk@gmail.com>
2025-05-18 02:13:06 +0200

    add doc source

--- pero_layout/pipeline/run.sh
+++
@@ -1,7 +0,0 @@
-python3 ./pero-ocr/user_scripts/parse_folder.py  \
-         -i ./images/ \
-         --output-alto-path ./output/alto \
-         -c ./pero-ocr/engine/config.ini \
-         --device cpu
-
-python3 ./processing/ocr-to-json.py ./output/alto/ ./images/
